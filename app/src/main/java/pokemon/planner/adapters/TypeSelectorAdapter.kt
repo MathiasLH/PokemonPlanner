4700bb014efc99325b30d2138f47499ed8596e1c
Mathias L. H <s144229@student.dtu.dk>
2019-10-29 09:30:49 +0100

    Fixed type selector (visually).

--- app/src/main/java/pokemon/planner/adapters/TypeSelectorAdapter.kt
+++ app/src/main/java/pokemon/planner/adapters/TypeSelectorAdapter.kt
@@ -2,6 +2,7 @@ package pokemon.planner.adapters
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
@@ -15,6 +16,7 @@ import pokemon.planner.model.TYPE
 
 class TypeSelectorAdapter(private val context: Context, private val types: ArrayList<TYPE>) : RecyclerView.Adapter<TypeSelectorAdapter.ViewHolder>() {
     private var currentType: TYPE = TYPE.NONE
+    private var lastPressedType: TYPE? = TYPE.NONE
     private var lastPressedHolder: ViewHolder? = null
 
     override fun onCreateViewHolder(
@@ -29,36 +31,28 @@ class TypeSelectorAdapter(private val context: Context, private val types: Array
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         holder.typeName.setText(types.get(position).toString())
-        val defaultShape = GradientDrawable()
-        defaultShape.setStroke(5, Color.GRAY)
-        val selectedShape = GradientDrawable()
-        selectedShape.setStroke(10, Color.BLACK)
-        defaultShape.cornerRadius = 8f
-        selectedShape.cornerRadius = 8f
-
-        defaultShape.setColor(Color.parseColor(types.get(position).color))
-        selectedShape.setColor(Color.parseColor(types.get(position).color))
-        holder.typeName.setOnClickListener {
-            if(lastPressedHolder != null){
-                val test = GradientDrawable()
-                //test = lastPressedHolder!!.typeName.background
-                var color: Int = Color.TRANSPARENT
-
-                var background: Drawable = lastPressedHolder!!.typeName.background
-
-                lastPressedHolder!!.typeName.background = defaultShape
-            }
-            Toast.makeText(context, types.get(position).toString(), Toast.LENGTH_LONG).show()
-            currentType =types.get(position)
-            println(currentType)
-            lastPressedHolder = holder
-        }
-        if(types.get(position) == currentType){
-            holder.typeName.background = selectedShape
+        val unSelected = GradientDrawable()
+        val selected = GradientDrawable()
+        unSelected.setStroke(5, Color.GRAY)
+        selected.setStroke(8, Color.BLACK)
+        unSelected.cornerRadius = 8f
+        selected.cornerRadius = 8f
+        unSelected.setColor(Color.parseColor(types.get(position).color))
+        selected.setColor(Color.parseColor(types.get(position).color))
+        if(types.get(position).equals(currentType)){
+            holder.typeName.background = selected
         }else{
-            holder.typeName.background = defaultShape
+            holder.typeName.background = unSelected
         }
+
+        holder.typeName.setOnClickListener {
+            holder.typeName.background = selected
+            currentType = types.get(position)
+            this.notifyDataSetChanged()
+        }
+
     }
 
 
